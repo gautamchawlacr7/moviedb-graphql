a0dd37d0d965509e3cea9a229e37ac52c37717e7
unknown <gautamchawla1999@gmail.com>
2020-08-16 21:51:33 +0530

    Model Changes

--- movies/apps.py
+++ movies/apps.py
@@ -2,9 +2,16 @@ from django.apps import AppConfig
 from tmdbv3api import Movie, Account, Authentication, TMDb
 import os
 
+popular = None
+latest = None
+upcoming = None
+
 class MoviesConfig(AppConfig):
     name = 'movies'
     def ready(self):
+        global popular
+        global latest
+        global upcoming
         tmdb = TMDb()
         tmdb.api_key = os.environ.get("API_KEY")
         movie = Movie()
@@ -12,4 +19,3 @@ class MoviesConfig(AppConfig):
         popular = movie.popular()
         latest = movie.latest()
         upcoming = movie.upcoming()
-
--- movies/models.py
+++ movies/models.py
@@ -7,11 +7,12 @@ class User(models.Model):
     updated_at = models.DateTimeField("updated at", auto_now=True)
     created_at = models.DateTimeField('created at', auto_now_add=True)
 
-class UserList(models.Model):
-    codename = models.CharField("Code Name", max_length=50, unique=True)
-    movie_list = models.JSONField("Movie List")
-
 class Movie(models.Model):
     id = models.IntegerField("Movie Id", primary_key=True)
     name = models.CharField("Movie Name", max_length=100)
     avg_rating = models.FloatField("Average Rating")
+
+class UserList(models.Model):
+    codename = models.CharField("Code Name", max_length=50, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie_list = models.ManyToManyField(Movie)
