00df1e0e7bf060b32ea6a3099b0c96c7a7da411a
unknown <gautamchawla1999@gmail.com>
2020-08-16 20:09:45 +0530

    Project Created and Configured

--- moviegraphql/settings.py
+++ moviegraphql/settings.py
@@ -18,10 +18,6 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
