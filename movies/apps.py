0eaf3b635236c7c176e25efa8febd1ad3224e238
unknown <gautamchawla1999@gmail.com>
2020-08-17 05:14:55 +0530

    Movies Schema Updated

--- movies/apps.py
+++ movies/apps.py
@@ -1,21 +1,4 @@
 from django.apps import AppConfig
-from tmdbv3api import Movie, Account, Authentication, TMDb
-import os
-
-popular = None
-latest = None
-upcoming = None
 
 class MoviesConfig(AppConfig):
     name = 'movies'
-    def ready(self):
-        global popular
-        global latest
-        global upcoming
-        tmdb = TMDb()
-        tmdb.api_key = os.environ.get("API_KEY")
-        movie = Movie()
-
-        popular = movie.popular()
-        latest = movie.latest()
-        upcoming = movie.upcoming()
--- movies/schema.py
+++ movies/schema.py
@@ -1,17 +1,37 @@
-"""import graphene
+import graphene
 from graphene_django import DjangoObjectType
-from .models import UserList, User
+from .models import Movie
+from django.db.models import Q
+from tmdbv3api import TMDb
+from tmdbv3api import Movie as mv
+import os
 
-class UserType(DjangoObjectType):
+class MovieType(DjangoObjectType):
     class Meta:
-        model = User
+        model = Movie
 
-
+#Query to get movies from the server
 class Query(graphene.ObjectType):
-    movies = graphene.List(Movie)
+    movies = graphene.List(MovieType, movie_id=graphene.Int())
 
-    def resolve_movies(self, info, **kwargs):
-        return Movie.objects.all()
+    def resolve_movies(self, info, movie_id=None, **kwargs):
+
+        if movie_id:
+
+            if Movie.objects.filter(pk=movie_id).exists():
+                return Movie.objects.filter(pk=movie_id)
+
+            else:
+                tmdb = TMDb()
+                tmdb.api_key = os.environ.get("API_KEY")
+                movie = mv()
+                searched_movie = movie.details(movie_id)
+                s_movie = Movie(id=searched_movie.id, name=searched_movie.title, avg_rating=searched_movie.vote_average)
+                s_movie.save()
+                return Movie.objects.filter(pk=movie_id)
+
+        movies = Movie.objects.all()
+        return movies
 
 class CreateMovie(graphene.Mutation):
     id = graphene.Int()
@@ -27,11 +47,11 @@ class CreateMovie(graphene.Mutation):
         movie = Movie(id=id, title=title, avg_rating=avg_rating)
         movie.save()
 
-        return CreateLink(
+        return CreateMovie(
             id=movie.id,
             title=movie.title,
             avg_rating=movie.avg_rating,
         )
 
 class Mutation(graphene.ObjectType):
-    create_movie = CreateMovie.Field()"""
+    create_movie = CreateMovie.Field()
