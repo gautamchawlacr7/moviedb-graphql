fd741aecf471f1c289a5d018ceaf51b7b99ce0f6
unknown <gautamchawla1999@gmail.com>
2020-08-17 22:48:17 +0530

    heroku db integration

--- moviegraphql/settings.py
+++ moviegraphql/settings.py
@@ -172,3 +172,9 @@ LOGGING = {
         },
     },
 }
+
+if os.environ.get('IS_HEROKU', 'False') == 'True':
+    import dj_database_url
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
