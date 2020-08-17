b1a90eca887fd3410a02b6ebc389af9018455f93
unknown <gautamchawla1999@gmail.com>
2020-08-17 15:59:56 +0530

    Authentication Added

--- moviegraphql/schema.py
+++ moviegraphql/schema.py
@@ -1,12 +1,15 @@
 import graphene
 import movies.schema
 import users.schema
+import graphql_jwt
 
 
 class Query(movies.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(movies.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- moviegraphql/settings.py
+++ moviegraphql/settings.py
@@ -136,4 +136,12 @@ STATIC_URL = '/static/'
 
 GRAPHENE = {
     'SCHEMA': 'moviegraphql.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
